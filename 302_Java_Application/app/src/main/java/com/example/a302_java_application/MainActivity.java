26022404d4542de32e021dd5eda7b3afe548c4a6
jhua479 <helenh121212@gmail.com>
2023-05-26 17:14:50 +1200

    Update DataProvider
    Added new value .xml file to store string which contain album data and updated DataProvider to use the data to create list of albums

--- 302_Java_Application/app/src/main/java/com/example/a302_java_application/MainActivity.java
+++ 302_Java_Application/app/src/main/java/com/example/a302_java_application/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.a302_java_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Album> allAlbums = new ArrayList<>();
+    ArrayList<Album> mostViewed = new ArrayList<>();
+    Context context = getApplicationContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//      Get the list of most viewed albums
+        this.mostViewed = getMostViewed();
+
+//      Create DataProvider to get albums
+        DataProvider dataProvider = new DataProvider(context);
+//      Get list of all albums and save to attribute
+        this.allAlbums = dataProvider.getAlbums();
+
+
+
+    }
+
+    public ArrayList<Album> getMostViewed() {
+
+        ArrayList<Album> mostViewed = new ArrayList<>();
+
+
+        return mostViewed;
     }
 }
