c0497a1d0ac64cbc9bb343586802ba7287ca1bc8
kylyn <swu628@aucklanduni.ac.nz>
2023-05-29 03:38:31 +1200

    display list of albums using recycler view

--- 302_Java_Application/app/src/main/java/com/example/a302_java_application/ListActivity.java
+++ 302_Java_Application/app/src/main/java/com/example/a302_java_application/ListActivity.java
@@ -1,14 +1,36 @@
 package com.example.a302_java_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ListActivity extends AppCompatActivity {
+
+    ArrayList<Album> allAlbums = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+//        Create DataProvider to get albums
+        DataProvider dataProvider = new DataProvider(this);
+//        Get list of all albums and save to attribute
+        this.allAlbums = dataProvider.getAlbums();
+
+//        Update list of all albums for DataProvider class
+        dataProvider.updateAlbumList(this.allAlbums);
+
+//        Create instance for recycler view
+        RecyclerView recyclerList = findViewById(R.id.list_recycler);
+//        Create instance for adapter for recycler view
+        RecyclerListAdapter recyclerListAdapter = new RecyclerListAdapter(this.allAlbums, this);
+//        Set adapter and layout manager for the recycler view
+        recyclerList.setAdapter(recyclerListAdapter);
+        recyclerList.setLayoutManager(new LinearLayoutManager(this));
     }
 }
