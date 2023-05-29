77fb180beb27be8d30c27d19fbbbdd1152578b4b
kylyn <swu628@aucklanduni.ac.nz>
2023-05-30 05:39:51 +1200

    implmented back button

--- 302_Java_Application/app/src/main/java/com/example/a302_java_application/DetailsActivity.java
+++ 302_Java_Application/app/src/main/java/com/example/a302_java_application/DetailsActivity.java
@@ -3,8 +3,10 @@ package com.example.a302_java_application;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -12,11 +14,13 @@ public class DetailsActivity extends AppCompatActivity {
 
      private ViewPager2 viewPager;
      ArrayList<Album> details;
-
+     private ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        back = (ImageView) findViewById(R.id.detailBack);
 
          // Initiating view pager
          viewPager = findViewById(R.id.pager);
@@ -29,11 +33,20 @@ public class DetailsActivity extends AppCompatActivity {
             details.add(album);
          }
 
+         // Display detail images using view pager
          ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this.details);
          viewPager.setAdapter(viewPagerAdapter);
          viewPager.setClipToPadding(false);
          viewPager.setClipChildren(false);
          viewPager.setOffscreenPageLimit(2);
          viewPager.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+         // Bring user back to the main activity when the back image is clicked
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
